149105d1c0fba9ed3a22b61e3b9c124053e55c53
SoniV53 <sonivega53@gmail.com>
2024-06-05 11:00:00 -0600

    use relese

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,16 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+    }
 }
